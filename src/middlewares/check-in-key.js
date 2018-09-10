8d9152d5de49c7c3501b0f4236dae1591c866d70
afrianjunior <afrian.junior26@gmail.com>
2018-09-10 10:48:49 +0700

    MODIFIED: change check key when production mode only

--- src/middlewares/check-in-key.js
+++ src/middlewares/check-in-key.js
@@ -4,14 +4,16 @@ import r from '../lib/resjson'
 export default () => {
   return (req, res, next) => {
     let response
-    if (!req.headers['x-authorization']) {
-      response = r('missing X-Authorization')
-      return res.status(status.BAD_REQUEST).json(response)
-    }
+    if (process.env.NODE_ENV === 'production') {
+      if (!req.headers['x-authorization']) {
+        response = r('missing X-Authorization')
+        return res.status(status.BAD_REQUEST).json(response)
+      }
 
-    if (req.headers['x-authorization'] !== process.env.TOKEN_KEY) {
-      response = r('X-Authorization not available')
-      return res.status(status.BAD_REQUEST).json(response)
+      if (req.headers['x-authorization'] !== process.env.TOKEN_KEY) {
+        response = r('X-Authorization not available')
+        return res.status(status.BAD_REQUEST).json(response)
+      }
     }
 
     return next()
